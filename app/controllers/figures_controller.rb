e68ca10ac0d9843f03dd0ed592fbf3dd4a947c85
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-09 15:05:57 -0400

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,56 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do 
+
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do   
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do 
+   figure = Figure.find(params[:id])
+   figure.update(params[:figure])
+
+   if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.create(params[:title])
+    end
+      
+    figure.save
+   redirect("/figures/#{figure.id}")
+  end
+
+  post '/figures' do 
+    figure =  Figure.find_or_create_by(name: params[:figure][:name])
+    title = Title.find_or_create_by(id: params[:figure][:title_ids])
+    figure.titles << title
+    
+    if params[:figure].include?("landmark_ids")
+      landmark = Landmark.find(params["figure"]["landmark_ids"]) 
+      figure.landmarks << landmark
+    elsif !params[:landmark][:name].empty?
+      new_landmark = Landmark.create(name: params[:landmark][:name])
+      figure.landmarks << new_landmark
+    end
+    figure.save
+  end
+
+end
+
+  
